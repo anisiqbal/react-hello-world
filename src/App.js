2faf45da83ad1dc832cc3bcadc2854e45288c91c
Anis Iqbal <anisiqbal484@gmail.com>
2020-02-24 17:13:58 +0500

    ReactJS Tutorial - 10 - State

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import {Greet} from './components/greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
-
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-       <Greet name="Bruce" heroName="Batman" />
+
+      <Message/>
+
+
+       {/* <Greet name="Bruce" heroName="Batman" />
        <Greet name="Clark" heroName="Superman"/>
        <Greet name="Diana" heroName="wonder Woman" />
-      {/* < Welcome/> */}
+
+      < Welcome name="Bruce" heroName="Batman" />
+      < Welcome name="Clark" heroName="Superman"/>
+      < Welcome name="Diana" heroName="wonder Woman" /> */}
       {/* <Hello/> */}
     </div>
   );
